189002dcb5069b593b28424354b074e5867a56d6
Ngoue David Roger Yannick <david.ngoue@outlook.fr>
2023-07-26 08:53:53 +0100

    Initial commit

---
+++ Blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from .views import *
+
+urlpatterns = [
+    path('', home, name="home"),
+    path('solution/', solution, name="solution"),
+    path('demo/', demo, name="demo"),
+    path('portfolio/', portfolio, name="portfolio"),
+    path('code/', code, name="code"),
+    path('contribution/', contribution, name="contribution"),
+]
---
+++ Blog/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, "home.html")
+
+def solution(request):
+    return render(request, "solution.html")
+
+def code(request):
+    return render(request, 'code.html')
+
+def demo(request):
+    return render(request, "demo.html")
+
+def portfolio(request):
+    return render(request, "portfolio.html")
+
+def contribution(request):
+    return render(request, "contribution.html")
